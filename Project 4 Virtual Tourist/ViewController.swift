6731be5a6f37c5916a2afa002d0b6399178187eb
mharpe2 <mharpe2@gmail.com>
2015-11-04 12:15:21 -0500

    Initial Commit

---
+++ Project 4 Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 4 Virtual Tourist
+//
+//  Created by Michael Harper on 11/4/15.
+//  Copyright © 2015 hxx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
